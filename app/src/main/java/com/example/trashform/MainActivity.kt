025b6e7bf66ce91845515aa25f5b44fde9db77d4
jendlrs <jensennics.dlrs@gmail.com>
2023-05-22 23:57:00 +0800

    Initial Commit, firebase setup is complete

---
+++ app/src/main/java/com/example/trashform/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.example.trashform
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    fun sendData(view: View) {
+        val database = Firebase.database
+        val myRef = database.getReference("message")
+
+        myRef.setValue("J&J FUTURA!")
+
+    }
+}
