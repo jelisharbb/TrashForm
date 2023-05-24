d7cd607700e617609b351f178966823b6630a2c2
jendlrs <jensennics.dlrs@gmail.com>
2023-05-24 17:38:02 +0800

    Reverted login to the main activity and added code to change the login page to the register page and vice versa

--- app/src/main/java/com/example/trashform/MainActivity.kt
+++ app/src/main/java/com/example/trashform/MainActivity.kt
@@ -1,22 +1,28 @@
 package com.example.trashform
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
+import android.widget.TextView
 import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var signUp: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-    }
 
-    fun sendData(view: View) {
-        val database = Firebase.database
-        val myRef = database.getReference("message")
+        signUp = findViewById(R.id.signUp)
 
-        myRef.setValue("J&J Futura!")
-
+        signUp.setOnClickListener {
+            val intent = Intent(this@MainActivity, RegisterActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
     }
 }
--- app/src/main/java/com/example/trashform/RegisterActivity.kt
+++ app/src/main/java/com/example/trashform/RegisterActivity.kt
@@ -1,11 +1,24 @@
 package com.example.trashform
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class RegisterActivity : AppCompatActivity() {
+
+    private lateinit var signIn: TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
+
+        signIn = findViewById(R.id.signIn)
+
+        signIn.setOnClickListener {
+            val intent = Intent(this@RegisterActivity, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
     }
 }
