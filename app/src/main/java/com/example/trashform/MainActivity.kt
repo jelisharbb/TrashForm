f286f67c11d3b0655ce50d53368efefa85cd1e65
Jelisha Ruth Bugnon <73575184+jelisharbb@users.noreply.github.com>
2023-05-26 19:35:05 +0800

    Added backend code for sign in

--- app/src/main/java/com/example/trashform/MainActivity.kt
+++ app/src/main/java/com/example/trashform/MainActivity.kt
@@ -6,23 +6,45 @@ import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import android.widget.TextView
+import android.widget.Toast
+import com.example.trashform.databinding.ActivityMainBinding
+import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var signUp: TextView
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var firebaseAuth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        signUp = findViewById(R.id.signUp)
-
-        signUp.setOnClickListener {
-            val intent = Intent(this@MainActivity, RegisterActivity::class.java)
+        firebaseAuth = FirebaseAuth.getInstance()
+        binding.signIn.setOnClickListener {
+            val intent = Intent(this, PrefaceActivity::class.java)
             startActivity(intent)
-            finish()
+        }
+
+        binding.signUp.setOnClickListener {
+            val email = binding.email.text.toString()
+            val pass = binding.password.text.toString()
+
+            if (email.isNotEmpty() && pass.isNotEmpty()) {
+
+                firebaseAuth.signInWithEmailAndPassword(email, pass).addOnCompleteListener {
+                    if (it.isSuccessful) {
+                        val intent = Intent(this, MainActivity::class.java)
+                        startActivity(intent)
+                    } else {
+                        Toast.makeText(this, it.exception.toString(), Toast.LENGTH_SHORT).show()
+                    }
+                }
+            } else {
+                Toast.makeText(this, "Empty fields are not allowed", Toast.LENGTH_SHORT).show()
+            }
         }
     }
 }
