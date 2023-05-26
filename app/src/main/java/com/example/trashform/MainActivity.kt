a362ad48c00a71cef46e6642fbabdf941cec14ff
jendlrs <jensennics.dlrs@gmail.com>
2023-05-26 21:37:15 +0800

    Override system night mode with light mode.

--- app/src/main/java/com/example/trashform/MainActivity.kt
+++ app/src/main/java/com/example/trashform/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.trashform
 
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
@@ -18,6 +19,7 @@ class MainActivity : AppCompatActivity() {
     private lateinit var firebaseAuth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
